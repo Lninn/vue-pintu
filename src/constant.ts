4a93e5e194265f6fc6a0c96a25533babda817578
Lninn <thl384458870@outlook.com>
2022-03-21 08:02:23 +0800

    refactor: rm file

--- src/constant.ts
+++
@@ -1,7 +0,0 @@
-export const CANVAS_SCLAE = 2
-
-export const RECT_WIDTH = 200 * CANVAS_SCLAE;
-export const RECT_HEIGHT = 200 * CANVAS_SCLAE;
-
-export const ROW_COUNT = 3;
-export const COL_COUNT = 3;
