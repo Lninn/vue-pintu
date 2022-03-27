ba369435eab46e1697b9a3048df788d20be8c3d1
Lninn <thl384458870@outlook.com>
2022-03-27 13:44:07 +0800

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,13 +4,6 @@ import { Manager } from './hepler'
 const IMAGE = 'https://cdn.pixabay.com/photo/2022/01/17/06/38/altai-6943982_960_720.jpg'
 const AUDIO_URL = 'https://img.tukuppt.com/newpreview_music/09/00/74/5c8949da6e66559783.mp3'
 
-interface Actions {
-  onReStart: () => void
-  onLevelChange: (level: string) => void
-  onFileChange: (file: any) => void
-  onAudioPlay: (txt: string) => any
-}
-
 export class AudioEffect {
   private audio: HTMLAudioElement | null = null
 
@@ -41,94 +34,6 @@ export class AudioEffect {
 
   clear() {
     this.audio = null
-  }
-}
-
-
-export class Control {
-  private stepEl: HTMLSpanElement | null = null
-  private stepCount: number = 0
-
-  actions: Actions
-
-  constructor(actions: Actions) {
-    this.actions = actions
-
-    this.initialize()
-  }
-
-  private initialize() {
-    const step = document.getElementById('step')
-    if (!step) return
-
-    this.stepEl = step as HTMLSpanElement
-
-    const reStart = document.getElementById('re-start')
-    if (!reStart) return
-
-    reStart.onclick = () => {
-      this.actions.onReStart()
-
-      this.stepCount = 0
-      this.drawStep()
-    }
-
-    const diff = document.getElementById('diff')
-    if (!diff) return
-
-    diff.onchange = (e: any) => {
-      const level = e.target.value
-      this.actions.onLevelChange(level)
-
-      this.stepCount = 0
-      this.drawStep()
-    }
-
-    const file = document.getElementById('file')
-    if (!file) return
-
-    const self = this
-    file.onchange = (e: any) => {
-      const file = e.target.files[0]
-      const reader = new FileReader()
-      reader.onload = createImage;
-      reader.readAsDataURL(file);
-
-      function createImage() {
-        const img = new Image();
-        img.onload = () => {
-          self.actions.onFileChange(img)
-        };
-        img.src = reader.result as any;
-      }
-    };
-
-    const audio = document.getElementById('audio')
-    if (!audio) return
-  
-    audio.onclick = () => {
-      const txt = audio.innerHTML
-      if (txt === '音效') {
-        audio.innerHTML = '静音'
-        this.actions.onAudioPlay('静音')
-      } else {
-        audio.innerHTML = '音效'
-        this.actions.onAudioPlay('音效')
-      }
-    }
-
-  }
-
-  recordStep() {
-    this.stepCount++
-
-    this.drawStep()
-  }
-
-  drawStep() {
-    if (this.stepEl) {
-      this.stepEl.innerHTML = this.stepCount.toString()
-    }
   }
 }
 
